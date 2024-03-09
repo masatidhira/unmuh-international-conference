72c8043a933f11e56e84373ce2eb1c0168bd7aa0
Atidhira Habibillah <masatidhira@gmail.com>
2024-03-09 13:38:24 +0700

    Update date

--- src/components/ImportantDates/ImportantDates.jsx
+++ src/components/ImportantDates/ImportantDates.jsx
@@ -6,10 +6,10 @@ function ImportantDates() {
             <h3 className="sectionTitle">Important Dates</h3>
             <h4 >Deadline</h4>
             <ul className="datesList">
-                <li>Full Papers Submission: <b>14 July 2024</b></li>
-                <li>Papers Acceptance Notification: <b>26 July 2024</b></li>
-                <li>Payment Registration: <b>31 August 2024</b></li>
-                <li>Upload Final Manuscript: 5 September 2024</li>
+                <li>Full Papers Submission: <b>20 June 2024</b></li>
+                <li>Papers Acceptance Notification: <b>20 July 2024</b></li>
+                <li>Payment Registration: <b>30 July 2024</b></li>
+                <li>Upload Final Manuscript: 1 August 2024</li>
                 <li>Conference Day: 24 Sebtember 2024</li>
             </ul>
         </section>
